211aea2cb922ce95333d0134dddb3fe315a8dc03
Ashl3y-Lin <164275529+Ashl3y-Lin@users.noreply.github.com>
2024-05-14 15:25:09 -0600

    Add files via upload

---
+++ testimonial.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.js-testimonials-slider', {
+  grabCursor: true,
+  spaceBetween:  30,
+  pagination:{
+    el: '.js-testimonials-pagination',
+    clickable: true
+  },
+  breakpoints:{
+    767:{
+      slidesPerView: 2
+    }
+  }
+
+
+
+});
